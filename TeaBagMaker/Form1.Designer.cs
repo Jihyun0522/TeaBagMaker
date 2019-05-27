c20bcd673abc890ead314883a8b8f40dc841ef56
jihyun0522 <s2017s01@e-mirim.hs.kr>
2019-05-27 09:51:29 +0900

    #1 폼 GUI 구성

--- TeaBagMaker/Form1.Designer.cs
+++ TeaBagMaker/Form1.Designer.cs
@@ -29,26 +29,27 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.combo = new System.Windows.Forms.ComboBox();
+            this.teaComboList = new System.Windows.Forms.ComboBox();
             this.txtCountDown = new System.Windows.Forms.TextBox();
             this.btnTea = new System.Windows.Forms.Button();
             this.timer = new System.Windows.Forms.Timer(this.components);
             this.label1 = new System.Windows.Forms.Label();
+            this.Time = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // combo
+            // teaComboList
             // 
-            this.combo.Font = new System.Drawing.Font("굴림", 12F);
-            this.combo.FormattingEnabled = true;
-            this.combo.Location = new System.Drawing.Point(12, 12);
-            this.combo.Name = "combo";
-            this.combo.Size = new System.Drawing.Size(241, 24);
-            this.combo.TabIndex = 0;
+            this.teaComboList.Font = new System.Drawing.Font("굴림", 12F);
+            this.teaComboList.FormattingEnabled = true;
+            this.teaComboList.Location = new System.Drawing.Point(12, 12);
+            this.teaComboList.Name = "teaComboList";
+            this.teaComboList.Size = new System.Drawing.Size(241, 24);
+            this.teaComboList.TabIndex = 0;
             // 
             // txtCountDown
             // 
             this.txtCountDown.Font = new System.Drawing.Font("굴림", 15F);
-            this.txtCountDown.Location = new System.Drawing.Point(13, 127);
+            this.txtCountDown.Location = new System.Drawing.Point(13, 155);
             this.txtCountDown.Name = "txtCountDown";
             this.txtCountDown.ReadOnly = true;
             this.txtCountDown.Size = new System.Drawing.Size(241, 30);
@@ -57,7 +58,7 @@
             // 
             // btnTea
             // 
-            this.btnTea.Location = new System.Drawing.Point(12, 55);
+            this.btnTea.Location = new System.Drawing.Point(13, 82);
             this.btnTea.Name = "btnTea";
             this.btnTea.Size = new System.Drawing.Size(241, 49);
             this.btnTea.TabIndex = 2;
@@ -71,21 +72,32 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(125, 173);
+            this.label1.Location = new System.Drawing.Point(125, 204);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(129, 12);
             this.label1.TabIndex = 3;
             this.label1.Text = "2019.05.27 3501 강지현";
             // 
+            // Time
+            // 
+            this.Time.AutoSize = true;
+            this.Time.Font = new System.Drawing.Font("굴림", 12F);
+            this.Time.Location = new System.Drawing.Point(12, 48);
+            this.Time.Name = "Time";
+            this.Time.Size = new System.Drawing.Size(145, 16);
+            this.Time.TabIndex = 4;
+            this.Time.Text = "차를 담그는 시간 : ";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(266, 196);
+            this.ClientSize = new System.Drawing.Size(266, 225);
+            this.Controls.Add(this.Time);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.btnTea);
             this.Controls.Add(this.txtCountDown);
-            this.Controls.Add(this.combo);
+            this.Controls.Add(this.teaComboList);
             this.Cursor = System.Windows.Forms.Cursors.IBeam;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
@@ -98,11 +110,12 @@
 
         #endregion
 
-        private System.Windows.Forms.ComboBox combo;
+        private System.Windows.Forms.ComboBox teaComboList;
         private System.Windows.Forms.TextBox txtCountDown;
         private System.Windows.Forms.Button btnTea;
         private System.Windows.Forms.Timer timer;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label Time;
     }
 }
 
--- TeaBagMaker/Form1.cs
+++ TeaBagMaker/Form1.cs
@@ -7,13 +7,22 @@ using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 
+using Microsoft.VisualBasic;
+
 namespace TeaBagMaker
 {
     public partial class Form1 : Form
     {
+        string[] teaList = new string[]{"홍차", "녹차", "루이보스차", "국화차"};
+        int[] teaTime = new int[]{120, 180, 300, 120 };
+        int SelectTea = 0;
+        int countNum = 0;
+
         public Form1()
         {
             InitializeComponent();
         }
+
+        
     }
 }
